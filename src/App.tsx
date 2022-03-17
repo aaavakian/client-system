1ce8dcc0d8f8fc341f732abac5ad537dc3385d6a
Armen Avakian <aaavakn@gmail.com>
2022-03-17 17:43:04 +0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+import './css/App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <h1>Welcome</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <span>Client Service</span>
+    </header>
+  );
+};
+
+export default Header;
